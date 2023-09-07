805de670b98e8d341091730f084f9d0c1830f83b
CG <144281059+godcindy@users.noreply.github.com>
2023-09-06 23:32:19 -0400

    Create my-solution.js

---
+++ my-solution.js
@@ -0,0 +1,28 @@
+```
+mkdir introductory-command-line-lab/
+pwd
+touch command.txt
+cd ~/introductory-command-line-lab
+cd
+ls
+history | tail -n 10
+```
+
+
+## Task 2
+
+Paste your solution to Problem 1 between the back ticks below.
+
+```
+~
+mkdir module-one/
+cd ..
+mkdir -p /module-one/introductory-javascript/variables/
+cd ..
+touch ./variables/var.txt
+touch ./variables/let.txt
+touch ./variables/const.txt
+cd ..
+open ./variables
+history | tail -n 10
+```
